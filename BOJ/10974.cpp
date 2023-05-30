591aa0ace4b47c065e3b41858852a7781830749b
DahyeonWoo <60066586+DahyeonWoo@users.noreply.github.com>
2023-05-30 12:54:08 +0900

    모든 순열 : 순열로 풂

---
+++ BOJ/10974.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+    int N;
+    vector<int> arr;
+
+    cin >> N;
+    for (int i = 1; i <= N; i++) {
+        arr.push_back(i);
+    }
+
+    do {
+       for (int i = 0; i < N; i++)
+            cout << arr[i] << ' ';
+        cout << '\n';
+    } while (next_permutation(arr.begin(), arr.end()));
+}
